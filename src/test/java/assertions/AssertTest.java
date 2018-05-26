8fc1ee5605b8e22caf0c3ad5c488c4d145c3e834
Anderson Ribeiro <anderson.moouro@gmail.com>
2018-05-26 11:38:06 -0300

    unit test

--- src/test/java/assertions/AssertTest.java
+++ src/test/java/assertions/AssertTest.java
@@ -100,4 +100,132 @@ public class AssertTest {
 
         org.junit.Assert.assertNotNull(domainNotification);
     }
+
+    @Test
+    public void should_not_return_notification_if_param_value_is_true() {
+
+        DomainNotification domainNotification = Assert.isTrue(true, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+    }
+
+    @Test
+    public void should_return_notification_if_param_value_is_false() {
+
+        DomainNotification domainNotification = Assert.isTrue(false, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNotNull(domainNotification);
+    }
+
+    @Test
+    public void should_not_return_notification_if_first_argument_value_is_greater_than_the_second_one() {
+
+        DomainNotification domainNotification = Assert.isGreaterThan(2, 1, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+
+        domainNotification = Assert.isGreaterThan(1.01, 1.005, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+    }
+
+    @Test
+    public void should_return_notification_if_first_argument_value_is_equal_or_less_than_the_second_one() {
+
+        DomainNotification domainNotification = Assert.isGreaterThan(1, 1, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNotNull(domainNotification);
+
+        domainNotification = Assert.isGreaterThan(1, 2, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNotNull(domainNotification);
+
+        domainNotification = Assert.isGreaterThan(1.1, 1.1, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNotNull(domainNotification);
+
+        domainNotification = Assert.isGreaterThan(1.1, 1.2, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNotNull(domainNotification);
+
+    }
+
+    @Test
+    public void should_not_return_notification_if_first_argument_value_is_greater_or_equal_than_the_second_one() {
+
+        DomainNotification domainNotification = Assert.isGreaterOrEqualThan(1, 1, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+
+        domainNotification = Assert.isGreaterOrEqualThan(2.003, 2.003, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+
+        domainNotification = Assert.isGreaterOrEqualThan(2, 0, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+
+        domainNotification = Assert.isGreaterOrEqualThan(1.3, 1.2, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+
+    }
+
+    @Test
+    public void should_return_notification_if_first_argument_value_is_less_than_the_second_one() {
+
+        DomainNotification domainNotification = Assert.isGreaterOrEqualThan(1, 2, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNotNull(domainNotification);
+
+        domainNotification = Assert.isGreaterOrEqualThan(2.003, 2.004, "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNotNull(domainNotification);
+
+    }
+
+    @Test
+    public void should_not_return_notification_if_email_is_valid() {
+
+        DomainNotification domainNotification = Assert.emailIsValid("aa@sss.com", "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+    }
+
+    @Test
+    public void should_return_notification_if_email_is_not_valid() {
+
+        DomainNotification domainNotification = Assert.emailIsValid("@.aaa.com", "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNotNull(domainNotification);
+
+        domainNotification = Assert.emailIsValid("ddd.@@l.aaa.com", "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNotNull(domainNotification);
+
+    }
+
+    @Test
+    public void should_not_return_notification_if_regex_match() {
+
+        DomainNotification domainNotification = Assert.regexMatch("123-45-6789", "^(\\d{3}-?\\d{2}-?\\d{4})$", "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+
+    }
+
+    @Test
+    public void should_not_return_notification_if_url_is_valid() {
+
+        DomainNotification domainNotification = Assert.urlIsValid("http://localhost/", "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+
+        domainNotification = Assert.urlIsValid("https://localhost/", "NotificationKey", "NotificationValue");
+
+        org.junit.Assert.assertNull(domainNotification);
+
+    }
+
+
 }
